0c932b01f026cd8826a241e2594adf5e2e6442cd
Danie Roux <danie@danieroux.com>
2016-01-09 11:47:07 +0200

    Basic generation of todotxt file from Org Task agenda implemented.

---
+++ org-todotxt.el
@@ -0,0 +1,156 @@
+;;; org-todotxt.el --- One-way http://todotxt.net integration for Org-mode
+
+;; Copyright (C) 2016 Free Software Foundation, Inc.
+
+;; Author: Danie Roux <danie at danieroux dot com>
+;; Keywords: todotxt gtd todo
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; http://todotxt.net is a text file format that expresses 'Gettings
+;; Things Done' (https://en.wikipedia.org/wiki/Getting_Things_Done)
+;; org-todotxt maps Org-mode files to todotxt files and back.
+;;
+;; org-todotxt serves as an alternative to org-mobile with a specific
+;; focus on GTD, which allows it to make assumptions and simplify
+;; things.
+
+;;; Code:
+
+(defvar org-todotxt-create-agenda-function 'org-todo-list
+  "The function used to generate the list of TODO's for the todotxt file.")
+
+(defvar org-todotxt-get-projects-function 'org-todotxt-get-projects
+  "The function used to resolve the project(s) associated with an Org task.")
+
+(defvar org-todotxt-get-contexts-function 'org-todotxt-get-contexts
+  "The function used to resolve the contexts associated with an Org task.")
+
+(defun org-todotxt-push (todotxt-file-name)
+  "Push the Org file into the todotxt file."
+  (interactive)
+  
+  (call-interactively org-todotxt-create-agenda-function)
+
+  (let ((todotxt-buffer (generate-new-buffer " *todotxt temp file*" )))
+    (with-current-buffer org-agenda-buffer
+      (goto-char (point-min))
+      (while (not (eobp))
+        (if-let ((original-task-marker
+                  (or (get-text-property (point) 'org-hd-marker)
+                      (get-text-property (point) 'org-marker))))
+            (progn
+              (with-current-buffer todotxt-buffer
+                (insert (org-todotxt--convert-org-line-to-todotxt-line original-task-marker))
+                (newline))))
+        (forward-line))
+      (with-current-buffer todotxt-buffer
+        (write-region nil nil todotxt-file-name nil 0))
+      (kill-buffer todotxt-buffer))))
+
+(defun org-todotxt--camel-case-project-name (project-name)
+  "Convert PROJECT-NAME into todotxt format.
+
+todotxt expects project names to be one word, CamelCased."
+  (replace-regexp-in-string " " "" (capitalize project-name)))
+
+(defun org-todotxt-get-projects (original-task-marker)
+  "Resolves project that ORIGINAL-TASK-MARKER is a part of.
+
+Assumes that the containing header is the project."
+  (with-current-buffer (marker-buffer original-task-marker)
+    (goto-char (marker-position original-task-marker))
+    (org-up-heading-safe)
+    (let* ((headline (nth 4 (org-heading-components)))
+           (project-name (org-todotxt--camel-case-project-name headline)))
+      (format "+%s" project-name))))
+
+(defun org-todotxt-get-contexts (original-task-marker)
+  "Resolves all contexts ORIGINAL-TASK-MARKER is a part of.
+
+Uses the Org tags associated with this task."
+  (with-current-buffer (marker-buffer original-task-marker)
+    (goto-char (marker-position original-task-marker))
+    (if-let ((tags (org-get-tags)))
+        (mapconcat 'identity tags " ")
+      "")))
+
+(defun org-todotxt--convert-org-line-to-todotxt-line (original-task-marker)
+  "Turn marker ORIGINAL-TASK-MARKER to an Org file line into a todotxt line."
+  (with-current-buffer (marker-buffer original-task-marker)
+    (goto-char (marker-position original-task-marker))
+    (let ((headline (nth 4 (org-heading-components)))
+          (contexts (funcall org-todotxt-get-contexts-function original-task-marker))
+          (projects-names (funcall org-todotxt-get-projects-function original-task-marker)))
+      (format "%s %s %s" headline projects-names contexts))))
+
+;;; Tests:
+
+(defun org-todotxt--test-find-org-file ()
+  "Open the test Org file."
+  (find-file-noselect "todotxt-test.org"))
+
+(defun org-todotxt--test-get-marker (match-text)
+  "Return the marker for MATCH-TEXT in the test org file."
+  (let* ((org-gtd-buffer (org-todotxt--test-find-org-file))
+         (task-marker (with-current-buffer org-gtd-buffer
+                        (goto-char (point-min))
+                        (search-forward match-text)
+                        (point-marker))))
+    task-marker))
+
+(defun org-todotxt--test-get-marker-build-rocket ()
+  (org-todotxt--test-get-marker "Build a rocket"))
+
+(defun org-todotxt--test-get-marker-hire-intern ()
+  (org-todotxt--test-get-marker "Hire an intern"))
+
+(defun org-todotxt--test-create-agenda ()
+  "Setup a specific Org Agenda buffer for testing purposes."
+  (interactive)
+  (let* ((org-gtd-buffer (org-todotxt--test-find-org-file)))
+    (with-current-buffer org-gtd-buffer
+      (put 'org-agenda-files 'org-restrict (list (buffer-file-name (buffer-base-buffer))))
+      (org-todo-list)
+      (put 'org-agenda-files 'org-restrict nil))))
+
+(ert-deftest org-todotxt-test--camel-case-project-name ()
+  (should (equal (org-todotxt--camel-case-project-name "Get to Mars")
+                 "GetToMars")))
+
+(ert-deftest org-todotxt-test--get-projects ()
+  (should (equal (org-todotxt-get-projects (org-todotxt--test-get-marker-build-rocket))
+                 "+GetToMars")))
+
+(ert-deftest org-todotxt-test--get-contexts ()
+  (should (equal (org-todotxt-get-contexts (org-todotxt--test-get-marker-build-rocket))
+                 "@crypt @lab"))
+  (should (equal (org-todotxt-get-contexts (org-todotxt--test-get-marker-hire-intern))
+                 "")))
+  
+(ert-deftest org-todotxt-test--convert-org-line-to-todotxt-line ()
+  (should (equal (org-todotxt--convert-org-line-to-todotxt-line (org-todotxt--test-get-marker-build-rocket))
+                 "Build a rocket +GetToMars @crypt @lab")))
+
+(ert-deftest org-todotxt-test-push ()
+  (let ((org-todotxt-create-agenda-function 'org-todotxt--test-create-agenda))
+    (org-todotxt-push "todo-test.txt")))
+
+;; (ert t)
+
+(provide 'org-todotxt)
+;;; org-todotxt.el ends here
